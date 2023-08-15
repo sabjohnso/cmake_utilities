a25bb37b2063578bde0182a18a7b2d4b3acef2b7
Samuel B. Johnson <sabjohnso@yahoo.com>
2023-08-15 08:27:15 -0500

    :sparkles: [catch2]

---
+++ FindCatch2.cmake
@@ -0,0 +1,13 @@
+if(NOT catch2_FOUND)
+  include(dependency)
+  git_resolvable_dependency(
+    NAME catch2
+    GIT_REPOSITORY https://github.com/catchorg/Catch2.git
+    GIT_TAG v3.4.0)
+  if(NOT catch2_FOUND)
+    add_subdirectory(
+      ${catch2_SOURCE_DIR}
+      ${catch2_BINARY_DIR}
+      ${catch2_EXCLUDE_FROM_ALL})
+  endif()
+endif()
