ab8d36b7e1fc48066735492d7cb358b593866561
Samuel B. Johnson <sabjohnso@yahoo.com>
2024-07-28 17:08:54 -0500

    :bug: [Catch2]

--- FindCatch2.cmake
+++ FindCatch2.cmake
@@ -2,7 +2,8 @@ if(NOT catch2_FOUND)
   include(dependency)
   git_resolvable_dependency(
     NAME catch2
-    GIT_REPOSITORY https://github.com/catchorg/Catch2.git)
+    GIT_REPOSITORY https://github.com/catchorg/Catch2.git
+  GIT_TAG devel)
   if(NOT catch2_FOUND)
     set_property(DIRECTORY ${catch2_SOURCE_DIR} PROPERTY EXCLUDE_FROM_ALL ON)
   endif()
