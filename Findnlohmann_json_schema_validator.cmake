8b0486be87512b05b452e3526dbc4925f7bd4a1c
Samuel B. Johnson <sabjohnso@yahoo.com>
2024-07-28 14:54:14 -0500

    :wrench: [nlohmann_json_schema_validator]

--- Findnlohmann_json_schema_validator.cmake
+++ Findnlohmann_json_schema_validator.cmake
@@ -4,12 +4,11 @@ if(NOT nlohmann_json_schema_validator_FOUND)
     NAME nlohmann_json_schema_validator
     GIT_REPOSITORY https://github.com/pboettch/json-schema-validator.git)
   if(NOT nlohmann_json_schema_validator_FOUND)
-    add_subdirectory(
-      ${nlohmann_json_schema_validator_SOURCE_DIR}
-      ${nlohmann_json_schema_validator_BINARY_DIR}
-      ${nlohmann_json_schema_validator_EXCLUDE_FROM_ALL})
-
     if(TARGET nlohmann_json_schema_validator)
+      if(nlohmann_json_schema_validator_EXCLUDE_FROM_ALL)
+        set_property(DIRECTORY ${nlohmann_json_schema_validator_SOURCE_DIR}
+          PROPERTY EXCLUDE_FROM_ALL ON)
+      endif()
       set(nlohmann_json_schema_validator_FOUND TRUE)
     else()
       message(FATAL_ERROR "Could not resolve nlohmann_json_schema_validator")
