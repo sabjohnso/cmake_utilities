90edab7f369c59f108b0001a151a364a2a64d9fb
Samuel B. Johnson <sabjohnso@yahoo.com>
2024-07-29 17:37:05 -0500

    :pencil2: [nlohmann_json_schema_validator]

--- Findnlohmann_json_schema_validator.cmake
+++ Findnlohmann_json_schema_validator.cmake
@@ -2,7 +2,7 @@ if(NOT nlohmann_json_schema_validator_FOUND)
   include(dependency)
   git_resolvable_dependency(
     NAME nlohmann_json_schema_validator
-    GIT_REPOSITORY https://github.com/pboettch/json-schema-validator.git)\
+    GIT_REPOSITORY https://github.com/pboettch/json-schema-validator.git)
   if(TARGET nlohmann_json_schema_validator)
     set(nlohmann_json_schema_validator_FOUND TRUE)
   else()
