fddda139565da578a83c5ec1aaad93a72c542d99
Samuel B. Johnson <sabjohnso@yahoo.com>
2024-07-29 17:35:22 -0500

    :bug: [nlohmann_json_schema_validator]

--- Findnlohmann_json_schema_validator.cmake
+++ Findnlohmann_json_schema_validator.cmake
@@ -2,12 +2,10 @@ if(NOT nlohmann_json_schema_validator_FOUND)
   include(dependency)
   git_resolvable_dependency(
     NAME nlohmann_json_schema_validator
-    GIT_REPOSITORY https://github.com/pboettch/json-schema-validator.git)
-  if(NOT nlohmann_json_schema_validator_FOUND)
-    if(TARGET nlohmann_json_schema_validator)
-      set(nlohmann_json_schema_validator_FOUND TRUE)
-    else()
-      message(FATAL_ERROR "Could not resolve nlohmann_json_schema_validator")
-    endif()
+    GIT_REPOSITORY https://github.com/pboettch/json-schema-validator.git)\
+  if(TARGET nlohmann_json_schema_validator)
+    set(nlohmann_json_schema_validator_FOUND TRUE)
+  else()
+    message(FATAL_ERROR "Could not resolve nlohmann_json_schema_validator")
   endif()
 endif()
