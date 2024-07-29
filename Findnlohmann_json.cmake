e387cd9a3a63193ac9f0cb80190e6d0d108414d9
Samuel B. Johnson <sabjohnso@yahoo.com>
2024-07-29 17:50:07 -0500

    :bug: [nlohmann_json]

--- Findnlohmann_json.cmake
+++ Findnlohmann_json.cmake
@@ -3,9 +3,4 @@ if(NOT nlohmann_json_FOUND)
   git_resolvable_dependency(
     NAME nlohmann_json
     GIT_REPOSITORY https://github.com/nlohmann/json.git)
-  if(nlohmann_json_POPULATED)
-    set(nlohmann_json_FOUND TRUE)
-  else()
-    message(FATAL_ERROR "Could not resolve nlohmann_json")
-  endif()
 endif()
